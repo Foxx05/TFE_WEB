19cec990b95396926e30deff4d4e159054061946
Foxx05 <theocolpaert8@gmail.com>
2026-04-25 13:57:41 +0200

    favicon + link page product

--- tfe/app/root.tsx
+++ tfe/app/root.tsx
@@ -18,7 +18,7 @@ export default function Root() {
     <html lang="en">
       <head>
         <Links/>
-        <link rel="icon" />
+        <link rel="icon" href="/logo.svg" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
