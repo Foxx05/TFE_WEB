6b79be27426901de12d1a3328dccc3caea84c33a
Foxx05 <theocolpaert8@gmail.com>
2026-04-25 12:53:22 +0200

    logo to svg + favicon

--- tfe/app/root.tsx
+++ tfe/app/root.tsx
@@ -18,7 +18,7 @@ export default function Root() {
     <html lang="en">
       <head>
         <Links/>
-        <link rel="icon" type="image/svg+xml" href={import.meta.env.BASE_URL + "logo.svg"} />
+        <link rel="icon" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
