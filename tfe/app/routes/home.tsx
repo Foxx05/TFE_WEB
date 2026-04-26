e496174f103bda6d0f3c8a58640d2f269276225f
Foxx05 <theocolpaert8@gmail.com>
2026-04-26 10:34:58 +0200

    section 1 -> comparaison

--- tfe/app/routes/home.tsx
+++ tfe/app/routes/home.tsx
@@ -14,6 +14,21 @@ export default function Home() {
           <h2>The future of strawberry greenhouse culture</h2>
         </div>
       </header>
+      <main>
+        <section className="gridcol">
+          <p className="section--title">From basic greenhouse...</p>
+          <p className="p--basic__modif">Everyone is familiar with the classic layout of a greenhouse. Generally, there are several rows of a certain type of fruit.</p>
+          <img className="img" src="/images/basicGH.png" alt="" />
+          <p className="p--basic">The rows are separated by buffer zones, allowing the farmer to move around. The farmer often has to check on the condition of the plants, which takes time.</p>
+        </section>
+
+        <section className="gridcol">
+          <p className="section--title">...to an automised one</p>
+          <p className="p--basic__modif">Our system allows the farmer to monitor the progress of the crops without having to move around.</p>
+          <img className="img" src="/images/autoGH.png" alt="" />
+          <p className="p--basic">Sensors transmit data directly to the farmer's phone. You receive all the information you need (temperature, humidity, etc.) in real time. This saves you time in terms of travel. You only need to leave your home if you are not satisfied with a particular parameter.</p>
+        </section>
+      </main>
     </main>
   );
 }
