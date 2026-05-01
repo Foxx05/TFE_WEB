f40f9b8f1f5fb582bffc46e1eda068b7ca81e51a
Foxx05 <theocolpaert8@gmail.com>
2026-05-01 19:06:06 +0200

    page app -> contenu

--- tfe/app/routes/contact.tsx
+++ tfe/app/routes/contact.tsx
@@ -12,18 +12,60 @@ export default function Home() {
           <h2>Get in touch with us for any enquiries and questions</h2>
       </header>
 
-      <section className="gridcol">
-        
-      </section>
+      <div className="contact--infos">
+          <div>
+            <h2>General inquiries</h2>
+            <p>berrycam.questions@gmail.com</p>
+            <p>+32 492 50 37 14</p>
+          </div>
 
-      <section className="gridcol">
-        
-      </section>
+          <div>
+            <h2>Collaborations</h2>
+            <p>berrycam.collab@gmail.com</p>
+            <p>+32 492 52 84 43</p>
+          </div>
+        </div>
 
-      <section className="gridcol">
-        
-      </section>
+        <form className="contact--form">
+          <label>
+            Name
+            <input type="text" name="name" />
+          </label>
 
+          <label>
+            Surname
+            <input type="text" name="surname" />
+          </label>
+
+          <label>
+            eMail Adress
+            <input type="email" name="email" />
+          </label>
+
+          <label>
+            Message
+            <textarea name="message"></textarea>
+          </label>
+
+          <div className="contact--checks">
+            <label>
+              <input type="checkbox" />
+              Send me a copy of my message
+            </label>
+
+            <label>
+              <input type="checkbox" />
+              Subscribe to the newsletter
+            </label>
+          </div>
+
+          <button className="btn--contact" type="submit">
+            Send it →
+          </button>
+        </form>
+
+        <img className="contact--img" src="/images/contact/strawberries.png" alt="Strawberries"/>
+        
       <footer>
         <Footer />
       </footer>
