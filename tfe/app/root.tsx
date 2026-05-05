464869cc7c65d44cdecc8fc9d3c6159ec3c4e277
Foxx05 <theocolpaert8@gmail.com>
2026-05-05 10:44:33 +0200

    meta partage + lien vers favicon ok

--- tfe/app/root.tsx
+++ tfe/app/root.tsx
@@ -19,11 +19,21 @@ export default function Root() {
     <html lang="en">
       <head>
         <Links/>
-        <link rel="icon" href="/logo.svg" />
+        <link rel="icon" href={`${import.meta.env.BASE_URL}/logo.ico`} />
+
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="My final project in my senior year of the Web and Transmedia program at HEAJ" />
+        <meta property="og:site_name" content="Berrycam — Théo Colpaert" />
+        <meta property="og:title" content="Berrycam" />
+        <meta property="og:description" content="My final project in my senior year of the Web and Transmedia program at HEAJ" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://theocolpaert.be/projets/tfe_web/index.html" />
+        <meta property="og:image" content="https://theocolpaert.be/projets/tfe_web/images/product/imgHeadProduct.png" />
+        <meta property="og:image:width" content="1800" />
+        <meta property="og:image:height" content="945" />
 
-        <title>TFE</title>
+        <title>Berrycam - TFE</title>
       </head>
       <body>
         <ScrollToTop />
