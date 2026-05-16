472b17fd4c557a94c18897b25f64bfe5c2a711c4
Foxx05 <theocolpaert8@gmail.com>
2026-05-16 11:53:01 +0200

    pagehistoire -> contenu ajouté + anim carousel

--- tfe/app/routes/history.tsx
+++ tfe/app/routes/history.tsx
@@ -38,8 +38,18 @@ export default function Home() {
       </section>
 
       <section className="section--page gridcol">
+        <p className="p--basic__number right">/03</p>
+        <p className="section--title__app right">Customer's needs</p>
+        <img className="img--right" src={`${import.meta.env.BASE_URL}/images/history/visit1.webp`} srcSet={`${import.meta.env.BASE_URL}/images/history/@2x/visit1@2x.webp 2x`} alt="" />
+        <div className="div--right">
+          <p className="p--basic__modif">I visited a strawberry farm in order to better understand the real needs and expectations of potential users of the&nbsp;application.</p>
+          <p className="p--basic">This visit allowed me to observe the farmers’ working environment and better understand the challenges of monitoring crop production and greenhouse conditions. By discussing directly with the growers, I gathered valuable feedback that helped me design an application better adapted to real agricultural needs, with a focus on usability and&nbsp;efficiency.</p>
+        </div>
+      </section>
+
+      <section className="section--page gridcol">
         <div className="img--right2">
-          <p className="p--basic__number left">/03</p>
+          <p className="p--basic__number left">/04</p>
           <p className="section--title__app left">A first website design</p>
           <p className="p--basic__modif">The first thing I did in this project was thinking about the website’s&nbsp;design.</p>
           <p className="p--basic">I started by making some wireframes on Figma. I did a lot of variation between every&nbsp;test.</p>
@@ -47,7 +57,7 @@ export default function Home() {
       </section>
 
       <section className="section--page gridcol">
-        <p className="p--basic__number right">/04</p>
+        <p className="p--basic__number right">/05</p>
         <p className="section--title__app right">The thinking behind the working&nbsp;model</p>
         <img className="img--right" src={`${import.meta.env.BASE_URL}/images/history/sheet3.webp`} srcSet={`${import.meta.env.BASE_URL}/images/history/@2x/sheet3@2x.webp 2x`} alt="" />
         <div className="div--right">
@@ -57,14 +67,30 @@ export default function Home() {
       </section>
 
       <section className="section--page gridcol">
-        <p className="p--basic__number right2">/05</p>
+        <p className="p--basic__number right2">/06</p>
         <p className="section--title__app right2">Proof of&nbsp;concept</p>
         <img className="img--right2" src={`${import.meta.env.BASE_URL}/images/history/sheet3.webp`} srcSet={`${import.meta.env.BASE_URL}/images/history/@2x/sheet3@2x.webp 2x`} alt="" />
         <div className="div--right2">
-          <p className="p--basic__modif">To validate the idea, I have designed a mock-up to provide data to our database.</p>
+          <p className="p--basic__modif">To validate the idea, I have designed a mock-up to provide data to our&nbsp;database.</p>
           <p className="p--basic">The mock-up is made of 3 digital strawberry plants with fruits represented by RBG LED. The LED's colours are changed to represent the strawberry from flower to full red ripped fruit. Som e sensors have also been added to simulate the weather's conditions (tempereture, humidity and athmosphere&nbsp;pressure)</p>
         </div>
       </section>
+
+      <div className="carousel">
+        <div className="group">
+          <div className="card">1</div>
+          <div className="card">2</div>
+          <div className="card">3</div>
+          <div className="card">4</div>
+        </div>
+
+        <div aria-hidden className="group">
+          <div className="card">1</div>
+          <div className="card">2</div>
+          <div className="card">3</div>
+          <div className="card">4</div>
+        </div>
+      </div>
 
       <footer>
         <Footer />
