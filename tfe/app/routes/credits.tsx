5aab90b10046aeb3d24cc424941a02089362ca31
Foxx05 <theocolpaert8@gmail.com>
2026-05-17 17:58:00 +0200

    page credit contenu en + / +img

--- tfe/app/routes/credits.tsx
+++ tfe/app/routes/credits.tsx
@@ -22,12 +22,13 @@ export default function Home() {
       <Reveal className="reveal--left">
         <section className="section--page gridcol">
           <p className="p--basic__number right">/01</p>
-          <p className="section--title__app right">3D&nbsp;assets</p>
+          <p className="section--title__app right"> Some 3D&nbsp;assets</p>
           <img className="img--right" src={`${import.meta.env.BASE_URL}/images/credit/renders.webp`} srcSet={`${import.meta.env.BASE_URL}/images/credit/@2x/renders@2x.webp 2x`} alt="Close-up of an open notebook, revealing a page labeled “TFE” (the French equivalent of a final project). A Bic pen is also visible. The whole scene is set on a wooden table."/>
           <div className="div--right">
             <p className="p--basic__modif">I used two 3D assets in my greenhouse scene: the blue plastic bins and the wheelbarrow.</p>
+            <p className="p--basic">Both assets are free. I took the liberty of using them because they saved me time. I created the rest of the 3D scenes myself in&nbsp;Blender.</p>
             <p className="p--basic"><a href="https://free3d.com/fr/3d-model/dirty-wheelbarrow-low-poly-3d-model-410575.html?dd_referrer=" target="_blank" rel="noopener noreferrer">Link to the wheelbarrow asset</a></p>
-            <p className="p--basic"><a href="https://free3d.com/fr/3d-model/plastic-fruit-crate-v1--190535.html" target="_blank" rel="noopener noreferrer">Link to the plastic bins</a></p>
+            <p className="p--basic"><a href="https://free3d.com/fr/3d-model/plastic-fruit-crate-v1--190535.html" target="_blank" rel="noopener noreferrer">Link to the plastic bins asset</a></p>
           </div>
         </section>
       </Reveal>
@@ -35,11 +36,12 @@ export default function Home() {
       <Reveal className="reveal--left">
         <section className="section--page gridcol">
           <p className="p--basic__number right">/02</p>
-          <p className="section--title__app right">Lajajaj</p>
-          <img className="img--right" src={`${import.meta.env.BASE_URL}/images/history/sheet.webp`} srcSet={`${import.meta.env.BASE_URL}/images/history/@2x/sheet@2x.webp 2x`} alt="Close-up of an open notebook, revealing a page labeled “TFE” (the French equivalent of a final project). A Bic pen is also visible. The whole scene is set on a wooden table."/>
+          <p className="section--title__app right">With the help of&nbsp;YouTube</p>
+          <img className="img--right" src={`${import.meta.env.BASE_URL}/images/credit/carousel.webp`} srcSet={`${import.meta.env.BASE_URL}/images/credit/@2x/carousel@2x.webp 2x`} alt="Close-up of an open notebook, revealing a page labeled “TFE” (the French equivalent of a final project). A Bic pen is also visible. The whole scene is set on a wooden table."/>
           <div className="div--right">
-            <p className="p--basic__modif">I think it’s important to thank a few people without whom this project wouldn’t be what it is&nbsp;today.</p>
-            <p className="p--basic">Thank you to the strawberry farmer who let me take photos of his greenhouses, to my internship supervisor for his regular feedback throughout the project, and to my teachers and friends for their&nbsp;help.</p>
+            <p className="p--basic__modif">I ran into a few issues with the carousel section on the “History”&nbsp;page.</p>
+            <p className="p--basic">I followed this tutorial video, it helped me understand how to achieve the look I wanted. I then adjusted the various settings to match the project’s overall&nbsp;atmosphere.</p>
+            <p className="p--basic"><a href="https://www.youtube.com/watch?v=KD1Yo8a_Qis" target="_blank" rel="noopener noreferrer">Link to watch the same video</a>.</p>
           </div>
         </section>
       </Reveal>
@@ -51,7 +53,7 @@ export default function Home() {
           <img className="img--right" src={`${import.meta.env.BASE_URL}/images/history/sheet.webp`} srcSet={`${import.meta.env.BASE_URL}/images/history/@2x/sheet@2x.webp 2x`} alt="Close-up of an open notebook, revealing a page labeled “TFE” (the French equivalent of a final project). A Bic pen is also visible. The whole scene is set on a wooden table."/>
           <div className="div--right">
             <p className="p--basic__modif">I think it’s important to thank a few people without whom this project wouldn’t be what it is&nbsp;today.</p>
-            <p className="p--basic">Thank you to the strawberry farmer who let me take photos of his greenhouses, to my internship supervisor for his regular feedback throughout the project, and to my teachers and friends for their&nbsp;help.</p>
+            <p className="p--basic">Thank you to the strawberry farmer who let me take photos of his greenhouses (La Fraiserie de Franière), to my internship supervisor for his regular feedback throughout the project, and to my teachers and friends for their&nbsp;help.</p>
           </div>
         </section>
       </Reveal>
