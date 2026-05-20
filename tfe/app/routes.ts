60d102af670726147125770551d1314a5cf05ba3
Foxx05 <theocolpaert8@gmail.com>
2026-05-20 22:29:14 +0200

    mise en place htaccess -> f5 page app history.. ok

--- tfe/app/routes.ts
+++ tfe/app/routes.ts
@@ -1,9 +1,9 @@
-import { route, index } from "@react-router/dev/routes";
+import { route } from "@react-router/dev/routes";
 
 export default [
-  route("projets/tfe_web", "routes/home.tsx"),
-  route("projets/tfe_web/app", "routes/app.tsx"),
-  route("projets/tfe_web/history", "routes/history.tsx"),
-  route("projets/tfe_web/contact", "routes/contact.tsx"),
-  route("projets/tfe_web/credits", "routes/credits.tsx"),
+  route("/projets/tfe_web/", "routes/home.tsx"),
+  route("/projets/tfe_web/app", "routes/app.tsx"),
+  route("/projets/tfe_web/history", "routes/history.tsx"),
+  route("/projets/tfe_web/contact", "routes/contact.tsx"),
+  route("/projets/tfe_web/credits", "routes/credits.tsx"),
 ];
