cb1ec5c8bd80d9b91a30851ab0389f99ac6e1f81
Foxx05 <theocolpaert8@gmail.com>
2026-05-29 13:35:13 +0200

    precision sur ai page credit

--- tfe/app/routes/credits.tsx
+++ tfe/app/routes/credits.tsx
@@ -96,7 +96,7 @@ export default function Home() {
           <img className="img--right" src={`${import.meta.env.BASE_URL}/images/credit/videoSection.webp`} srcSet={`${import.meta.env.BASE_URL}/images/credit/@2x/videoSection@2x.webp 2x`} alt="Screenshot of the 3D greenhouse model that appears in the animated section."/>
           <div className="div--right">
             <p className="p--basic__modif">As for the app itself, I didn't rely on any external resources. I just browsed Dribbble looking for&nbsp;inspiration.</p>
-            <p className="p--basic">However, when it came to account management, that’s where things got complicated. In fact, I had never done that before. So I had to rely on YouTube and AI to achieve the current&nbsp;result.</p>
+            <p className="p--basic">However, when it came to account management, that’s where things got complicated. In fact, I had never done that before. So I had to rely on YouTube and AI (ChatGPT) to achieve the current&nbsp;result.</p>
             <p className="p--basic"><a href="https://dribbble.com/" target="_blank" rel="noopener noreferrer">Link to Dribbble</a></p>
             <p className="p--basic"><a href="https://www.youtube.com/watch?v=JAgd_L3GhI0" target="_blank" rel="noopener noreferrer">Link to watch the same video</a></p>
           </div>
